1c08cf9d3e4a22135052225932abca52e6ee8fcb
Jerry Shao <72317125+JerryShao3234@users.noreply.github.com>
2022-01-05 22:16:04 -0800

    Add files via upload

---
+++ RTOS_Scheduler/osKernel.h
@@ -0,0 +1,18 @@
+#ifndef _OS_KERNEL_H
+#define _OS_KERNEL_H
+#include "stm32f4xx.h"                  // Device header
+#include <stdint.h>
+void osKernelLaunch(uint32_t quanta);
+
+void osKernelInit(void);
+void osSignalWait(int32_t *semaphore);
+void osSignalSet(int32_t *semaphore);
+void osSemaphoreInit(int32_t *semaphore, int32_t value);			 
+void osThreadYield(void);
+uint8_t osKernelAddThreads(void(*task0)(void),
+													 void(*task1)(void),
+													 void(*task2)(void));
+
+#endif
+													 
+													 
